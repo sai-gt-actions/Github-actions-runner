1eca9d37c5e0867d71d150e6000fb2b2a7ac3598
sai goud <saikumarmogulla40@gmail.com>
2026-01-30 16:00:55 +0530

    github-actions

---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "project" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "zone_name" {
+  type        = string
+  default     = "saidaws86s.fun"
+  description = "description"
+}
+
+variable "zone_id" {
+  type        = string
+  default     = "Z0506429205Z4ZA4IXHNC"
+  description = "description"
+}
