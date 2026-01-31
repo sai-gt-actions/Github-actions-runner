83609f7f545aca7026f7ef039ee1df5635178fe4
sai goud <saikumarmogulla40@gmail.com>
2026-01-31 21:25:50 +0530

    github-actions

--- main.tf
+++ main.tf
@@ -1,49 +1,100 @@
 
 
-resource "aws_instance" "runner" {
-  ami           = local.ami_id
-  instance_type = "t3.small"
-  vpc_security_group_ids = [aws_security_group.main.id]
-  subnet_id = "subnet-0bf3d53e59ef42889" #replace your Subnet
+# resource "aws_instance" "runner" {
+#   ami           = local.ami_id
+#   instance_type = "t3.small"
+#   vpc_security_group_ids = [aws_security_group.main.id]
+#   subnet_id = "subnet-0bf3d53e59ef42889" #replace your Subnet
 
-  # need more for terraform
-  root_block_device {
-    volume_size = 50
-    volume_type = "gp3" # or "gp2", depending on your preference
+#   # need more for terraform
+#   root_block_device {
+#     volume_size = 50
+#     volume_type = "gp3" # or "gp2", depending on your preference
+#   }
+#   user_data = file("runner.sh")
+#   tags = merge(
+#     local.common_tags,
+#     {
+#         Name = "${var.project}-${var.environment}-runner"
+#     }
+#   )
+# }
+
+# resource "aws_security_group" "main" {
+#   name        =  "${var.project}-${var.environment}-runner"
+#   description = "Created to attatch runner"
+
+#   egress {
+#     from_port        = 0
+#     to_port          = 0
+#     protocol         = "-1"
+#     cidr_blocks      = ["0.0.0.0/0"]
+#     ipv6_cidr_blocks = ["::/0"]
+#   }
+
+#   ingress {
+#     from_port        = 0
+#     to_port          = 0
+#     protocol         = "-1"
+#     cidr_blocks      = ["0.0.0.0/0"]
+#     ipv6_cidr_blocks = ["::/0"]
+#   }
+
+#   tags = merge(
+#     local.common_tags,
+#     {
+#         Name = "${var.project}-${var.environment}-jenkins"
+#     }
+#   )
+# }
+
+# Security Group for Runner
+resource "aws_security_group" "runner" {
+  name        = "${var.project}-${var.environment}-runner-sg"
+  description = "SG for GitHub Actions runner to access private EKS API"
+  vpc_id      = aws_vpc.main.id
+
+  # Outbound: allow HTTPS to EKS control plane
+  egress {
+    from_port       = 443
+    to_port         = 443
+    protocol        = "tcp"
+    security_groups = [aws_security_group.eks_control_plane.id] # reference EKS SG
   }
-  user_data = file("runner.sh")
+
+  # Inbound: allow all internal VPC traffic
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = [var.vpc_cidr]
+  }
+
   tags = merge(
     local.common_tags,
-    {
-        Name = "${var.project}-${var.environment}-runner"
-    }
+    { Name = "${var.project}-${var.environment}-runner-sg" }
   )
 }
 
-resource "aws_security_group" "main" {
-  name        =  "${var.project}-${var.environment}-runner"
-  description = "Created to attatch runner"
+# EC2 Runner Instance
+resource "aws_instance" "runner" {
+  ami           = local.ami_id             # From locals.tf
+  instance_type = "t3.small"
 
-  egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
+  # Choose first private subnet automatically
+  subnet_id = element(var.private_subnet_ids, 0)
+
+  vpc_security_group_ids = [aws_security_group.runner.id]
+
+  root_block_device {
+    volume_size = 50
+    volume_type = "gp3"
   }
 
-  ingress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
+  user_data = file("runner.sh")
 
   tags = merge(
     local.common_tags,
-    {
-        Name = "${var.project}-${var.environment}-jenkins"
-    }
+    { Name = "${var.project}-${var.environment}-runner" }
   )
 }
--- variables.tf
+++ variables.tf
@@ -1,19 +1,51 @@
+# variable "project" {
+#     default = "roboshop"
+# }
+
+# variable "environment" {
+#     default = "dev"
+# }
+
+# variable "zone_name" {
+#   type        = string
+#   default     = "saidaws86s.fun"
+#   description = "description"
+# }
+
+# variable "zone_id" {
+#   type        = string
+#   default     = "Z0506429205Z4ZA4IXHNC"
+#   description = "description"
+# }
+
+variable "vpc_cidr" {
+  default = "10.0.0.0/16"
+}
+
+variable "private_subnet_ids" {
+  type    = list(string)
+  default = [
+    "subnet-0bf3d53e59ef42889", # 10.0.11.0/24
+    "subnet-0bf3d53e59ef42890"  # 10.0.12.0/24
+  ]
+}
+
 variable "project" {
-    default = "roboshop"
+  default = "roboshop"
 }
 
 variable "environment" {
-    default = "dev"
+  default = "dev"
 }
 
 variable "zone_name" {
   type        = string
   default     = "saidaws86s.fun"
-  description = "description"
+  description = "Route53 zone name"
 }
 
 variable "zone_id" {
   type        = string
   default     = "Z0506429205Z4ZA4IXHNC"
-  description = "description"
+  description = "Route53 zone ID"
 }
